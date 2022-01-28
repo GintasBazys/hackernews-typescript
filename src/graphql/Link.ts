9a7b6181d7e03b8b22fc5cad098cd67d0dda9f37
gintas <bazys.gintas@gmail.com>
2022-01-28 13:00:15 +0200

    Mutions - REST

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { objectType, extendType } from "nexus";
+import { objectType, extendType, nonNull, stringArg, idArg, list } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
@@ -30,6 +30,69 @@ export const LinkQuery = extendType({
       type: "Link",
       resolve(parent, args, context, info) {
         return links;
+      },
+    });
+    t.field("link", {
+      type: "Link",
+      args: {
+        id: nonNull(idArg()),
+      },
+      resolve(parent, args, context, info) {
+        const { id } = args;
+        const link = links.find((link) => link.id.toString() === id);
+        return link;
+      },
+    });
+  },
+});
+
+export const LinkMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("post", {
+      type: "Link",
+      args: {
+        description: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+      },
+      resolve(parent, args, context) {
+        const { description, url } = args;
+        let idCount = links.length + 1;
+        const link = {
+          id: idCount,
+          description: args.description,
+          url: args.url,
+        };
+        links.push(link);
+        return link;
+      },
+    });
+    t.nonNull.field("updateLink", {
+      type: "Link",
+      args: {
+        id: nonNull(idArg()),
+        url: stringArg(),
+        description: stringArg(),
+      },
+      resolve(parent, args, context) {
+        const { id, url, description } = args;
+        const link = links.find((link) => link.id.toString() === id);
+        link?.url = url;
+        link?.description = description;
+        return link;
+      },
+    });
+    t.nonNull.field("deleteLink", {
+      type: Link,
+      args: {
+        id: nonNull(idArg()),
+      },
+      resolve(parent, args, context) {
+        const { id } = args;
+        const index = links.findIndex((link) => link.id.toString() === id);
+        const link = links[index];
+        links.splice(index, 1);
+        return link;
       },
     });
   },
